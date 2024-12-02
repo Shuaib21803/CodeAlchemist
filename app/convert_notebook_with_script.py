74808d7b065c1f38fd8461c825f8ffbc076f949f
Arnav Rajurkar <arnavrajurkar@gmail.com>
2024-12-02 15:36:58 +0530

    Made a new function to convert the notebook directly into a .py file and hence reducing the workload of Gemini

---
+++ app/convert_notebook_with_script.py
@@ -0,0 +1,23 @@
+import nbformat
+from nbconvert import PythonExporter
+
+def convert(notebook_path):
+    with open(notebook_path, "r", encoding="utf-8") as f:
+        notebook_content = nbformat.read(f, as_version=4)
+
+    exporter = PythonExporter(template_name="python")
+    script, _ = exporter.from_notebook_node(notebook_content)
+
+    output_path = notebook_path.replace(".ipynb", ".py")
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(script)
+
+    print(f"Converted {notebook_path} to {output_path}")
+
+
+convert("notebook/2.ipynb")
+
+
+
+
+
